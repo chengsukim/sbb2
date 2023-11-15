eabb967aab147658f462d33ddc0c95b875a73f69
깃chengsukim <kcs981123@naver.com>
2023-11-15 12:27:50 +0900

    sbb2

---
+++ src/main/java/com/sdd/sbb3/MainController.java
@@ -0,0 +1,14 @@
+package com.sdd.sbb3;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/main")
+    @ResponseBody
+    public String showmain() {
+        return "Hello Sbb";
+    }
+}
---
+++ src/test/java/com/sdd/sbb3/Sbb3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdd.sbb3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
